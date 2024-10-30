6e93a2c1d18b19627cd3539da292b8a3d375d570
JimmyHu <60071jimmy@gmail.com>
2024-10-30 12:16:37 +0800

    Update video_processing.py
    Update video_processing.py

--- video_processing.py
+++ video_processing.py
@@ -6,6 +6,7 @@ import cv2
 import ffmpeg
 import numpy as np
 import torch
+from skimage import img_as_ubyte
 
 def process_video_frame_by_frame(input_file, output_file, model_restoration):
     """
