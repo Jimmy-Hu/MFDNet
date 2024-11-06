74fab46f22ebdc5d36f6310b093e3b0ba92098b0
JimmyHu <60071jimmy@gmail.com>
2024-11-06 12:33:57 +0800

    Update video_processing.py
    Update video_processing.py

--- video_processing.py
+++ video_processing.py
@@ -19,3 +19,5 @@ def process_video_frame_by_frame(input_file, output_file, model_restoration):
     """
     Decodes a video frame by frame, processes each frame,
     and re-encodes to a new video.
+
+    Args:
