6d6d78ce33548dbb865338890a23a7b8494a104d
JimmyHu <60071jimmy@gmail.com>
2024-11-21 20:55:54 +0800

    Update video_processing.py
    Update video_processing.py

--- video_processing.py
+++ video_processing.py
@@ -36,3 +36,4 @@ def process_video_frame_by_frame(input_file, output_file, model_restoration):
             ffmpeg
             .input(input_file)
             .output('pipe:', format='rawvideo', pix_fmt='rgb24')
+            .run_async(pipe_stdout=True)
