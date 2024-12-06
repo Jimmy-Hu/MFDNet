892670aee06953c29e1bb91eac62e35f5f6e7ac9
JimmyHu <60071jimmy@gmail.com>
2024-12-06 10:30:06 +0800

    Update video_processing.py
    Update video_processing.py

--- video_processing.py
+++ video_processing.py
@@ -52,3 +52,4 @@ def process_video_frame_by_frame(input_file, output_file, model_restoration):
         while in_bytes := process1.stdout.read(width * height * 3):
             with torch.no_grad(): 
                 in_frame = torch.frombuffer(in_bytes, dtype=torch.uint8).float().reshape((1, 3, width, height))
+                in_frame_gpu = torch.div(in_frame, 255).to(device='cuda')
