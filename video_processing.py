e40a2a5ef5e315e03490fd179e172c8eca69399d
JimmyHu <60071jimmy@gmail.com>
2024-12-21 23:31:30 +0800

    Update video_processing.py
    Update video_processing.py

--- video_processing.py
+++ video_processing.py
@@ -67,3 +67,4 @@ def process_video_frame_by_frame(input_file, output_file, model_restoration):
                 process2.stdin.write(out_frame.tobytes())
 
         # Close streams
+        process1.stdout.close()
