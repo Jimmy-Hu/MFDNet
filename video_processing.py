f3af1516fcd5d3e1ebd7a0f35f711f05519a5b6e
JimmyHu <60071jimmy@gmail.com>
2025-01-17 13:21:50 +0800

    Update video_processing.py
    Update video_processing.py

--- video_processing.py
+++ video_processing.py
@@ -94,3 +94,4 @@ if __name__ == '__main__':
     utils.load_checkpoint(model_restoration, args.weights)
     print("===>Testing using weights: ", args.weights)
 
+    model_restoration.eval().cuda()
