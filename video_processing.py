95a451394d012cb61f3220e5c710fa4dfbc99b5d
JimmyHu <60071jimmy@gmail.com>
2025-03-10 12:35:28 +0800

    Update video_processing.py
    Update video_processing.py

--- video_processing.py
+++ video_processing.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
 
     model_restoration.eval().cuda()
     
-    input_video = "Input_Video."
+    input_video = "Input_Video.m"
     output_video = "Output_Video.mp4"
 
     process_video_frame_by_frame(input_video, output_video, model_restoration)
